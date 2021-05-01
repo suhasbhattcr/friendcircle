e8f3e2611073ff5651c59d7357bd094401ff67c0
suhasbhattcr <suhas.bhatt@outlook.com>
2021-05-01 21:04:25 +0530

    updated cors

--- friends/src/main.ts
+++ friends/src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { config } from './config/swagger.config';
-
+import { corsConfig } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsConfig);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3200);
 }
 bootstrap();
